c65566724c16ec18597e2ba6c1a76363c17f3d24
Allen Mathews <allenjoemathews@gmail.com>
2023-09-19 11:22:15 +1000

    added troll face image

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <h1>This is a Header</h1>
+            <img src='./images/troll-face.png' alt='a black and white troll face'/>
+            <h2>Header Component</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+    )
+}
