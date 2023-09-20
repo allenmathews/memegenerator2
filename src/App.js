2fc2c018fadc47486c61e2b68146e8ad327c56c1
Allen Mathews <allenjoemathews@gmail.com>
2023-09-20 21:42:13 +1000

    added state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Meme from './components/Meme';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <Meme />
       </header>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,9 @@ import React from 'react'
 export default function Header() {
     return (
         <header className='header'>
-            <h1>This is a Header</h1>
-            <img src='./images/troll-face.png' alt='a black and white troll face'/>
-            <h2>Header Component</h2>
-            <h4>React Course - Project 3</h4>
+            <img src='./images/troll-face.png' alt='a black and white troll face' className='header--image'/>
+            <h3 className='header--title'>Meme Generator</h3>
+            <h3 className='header--project'>React Course - Project 3</h3>
         </header>
     )
 }
