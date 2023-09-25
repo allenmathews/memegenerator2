9d2045ba541b02314c95d367ff9dbfcf52354ceb
Allen Mathews <allenjoemathews@gmail.com>
2023-09-25 20:05:36 +1000

    ad

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
+//header
 export default function Header() {
-    return (
-        <header className='header'>
-            <img src='./images/troll-face.png' alt='a black and white troll face' className='header--image'/>
-            <h3 className='header--title'>Meme Generator</h3>
-            <h3 className='header--project'>React Course - Project 3</h3>
-        </header>
+    return ( <
+        header className = 'header' >
+        <
+        img src = './images/troll-face.png'
+        alt = 'a black and white troll face'
+        className = 'header--image' / >
+        <
+        h3 className = 'header--title' > Meme Generator < /h3> <
+        h3 className = 'header--project' > React Course - Project 3 < /h3> <
+        /header>
     )
 }
