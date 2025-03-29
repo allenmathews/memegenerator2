5195334a6515f187192f309071e7463e8c07ce11
Allen Mathews <allenjoemathews@gmail.com>
2025-03-29 21:07:43 +1100

    done

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Meme() {
     const [meme, setMeme] = React.useState({
-        topText: "",
+        topText: "blank",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg" 
     })
